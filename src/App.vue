78388d7780b750523861ec577653ab7a6ab7252c
Kyrpychenko <denniskyrpychenko@gmail.com>
2024-05-02 16:34:29 +0200

    wip: guess function

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <template>
-  {{ solutionNumber }}
   <div class="d-flex justify-content-center align-items-center h-100">
     <div class="card p-3" v-if="start">
-      <div class="card-title">Number Guessing Game</div>
       <div class="card-body">
+        <div class="card-title">Number Guessing Game</div>
         Enter the range the numbers should have
         <form @submit="startGame()">
           <div class="d-flex">
@@ -24,12 +23,18 @@
     </div>
 
     <div class="card p-3" v-else>
-      <div class="card-title">Number Guessing Game</div>
       <div class="card-body">
-        Guess the random number
-        <form @submit="guess()">
+        <div class="card-title">Number Guessing Game</div>
+        <span v-if="!guessMessage">Guess the random number</span>
+        <span v-else>{{ guessMessage }}</span>
+        <form @submit.prevent="guess()">
           <div class="d-flex">
-            <input type="number" class="form-control" />
+            <input
+              step="1"
+              type="number"
+              v-model="guessNumber"
+              class="form-control"
+            />
             <button class="btn btn-primary">Submit</button>
           </div>
         </form>
@@ -45,6 +50,8 @@ const start = ref(true);
 const firstRangeNumber = ref(0);
 const secondRangeNumber = ref(0);
 const solutionNumber = ref(0);
+const guessNumber = ref(0);
+const guessMessage = ref("");
 
 function startGame() {
   solutionNumber.value = Math.floor(
@@ -54,5 +61,15 @@ function startGame() {
   start.value = false;
 }
 
-function guess() {}
+function guess() {
+  if (guessNumber.value == solutionNumber.value) {
+    guessMessage.value = "you won!";
+    // win function, show the solution number, restart button
+  } else if (guessNumber.value < solutionNumber.value) {
+    guessMessage.value = "the solution is bigger";
+  } else {
+    guessMessage.value = "the solution is smaller";
+  }
+  guessNumber.value = 0;
+}
 </script>
