6ffdd46f6047866ad74853600c4a7a6493f1d2fd
TimScholtz <t.scholz@mbd-team.de>
2024-05-02 15:46:32 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="d-flex justify-content-center align-items-center h-100">
+    <div class="card p-3">
+      <div class="card-title">Number Guessing Game</div>
+      <div class="card-body">
+        <div class="d-flex">
+          <input type="number" class="form-control" />
+          <button class="btn btn-primary">TEST</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped></style>
+<script setup lang="ts"></script>
